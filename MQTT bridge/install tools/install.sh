ff9c8bbf7331333d2d88fd59bfdd35ff18a51e62
Raging Bits <84232464+RagingBits@users.noreply.github.com>
2022-08-01 23:01:18 +0100

    Create install.sh

---
+++ MQTT bridge/install tools/install.sh
@@ -0,0 +1,8 @@
+apt-get install python3
+apt-get install python3-pip
+python3 -m pip install --upgrade pip
+apt-get install python3-setuptools
+apt-get install python3-wheel
+python3 -m pip install paho-mqtt
+python3 -m pip install pyserial
+apt-get install mosquitto
